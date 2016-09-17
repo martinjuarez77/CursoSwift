f3771bedda89cb5569141c0f30cf1e6f8ea23a3b
martincho <martinjuarez77@gmail.com>
2016-09-17 13:51:14 +0200

    Tarea Curso Swift - Velocímetro

--- TareaSemana2.playground/Contents.swift
+++ TareaSemana2.playground/Contents.swift
@@ -1,29 +1,18 @@
-//: Curso SWIFT - Tarea Semana 2 - Martín Juarez
+let rango = 0...100
 
-// Creamos un rango de números del 0 al 100 incluídos ambos
-var rangoNumeros = 0...100
-
-// iteramos por cada elemento de la colección
-for numero in rangoNumeros{
-
-    // Si el número es divisible entre 5, imprime: # el número  + "Bingo!!!"
-    if (numero % 5 == 0 && numero > 0){
-        print ("\(numero) Bingo!!!")
+for indice in rango {
+    
+    if indice % 5 == 0 {
+        print ("#El número \(indice) Bingo!!!")
     }
     
-    // Si el número es par, imprime: # el número + "par!!!"
-    if (numero % 2 == 0 && numero > 0){
-        print ("\(numero) par!!!")
+    if indice % 2 == 0 {
+        print ("#El número \(indice) par!!!")
+    }else {
+        print ("#El número \(indice) impar!!!")
     }
     
-    // Si el número es impar, imprime: # el número + "impar!!!"
-    if (numero % 2 != 0){
-        print ("\(numero) impar!!!")
+    if indice >= 30 && indice <= 40 {
+        print ("#El número \(indice) Viva Swift!!!")
     }
-    
-    // Si el número se encuentra dentro de un rango del 30 al 40, imprime: # el número +  "Viva Swift!!!"
-    if (numero >= 30 && numero <= 40) {
-        print ("\(numero) Viva Swift!!!")
-    }
-    
 }
